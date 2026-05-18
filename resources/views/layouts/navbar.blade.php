04992757be87e801040eff0ed31e6bec88ae75fb
Aman <amanyadav923949@gmail.com>
2026-05-18 15:03:40 +0530

    fix: remove unnecessary closing @endif for reports link in navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -26,7 +26,6 @@
                         <li class="nav-item"><a class="nav-link {{ request()->routeIs('reports.*') ? 'active' : '' }}" href="{{ route('reports.index') }}">Reports</a></li>
                     @endif
                     <li class="nav-item"><a class="nav-link {{ request()->routeIs('notifications.*') ? 'active' : '' }}" href="{{ route('notifications.index') }}">Notifications</a></li>
-                    @endif
                 @endauth
             </ul>
             <div class="d-flex gap-2 ms-lg-3 mt-3 mt-lg-0">
